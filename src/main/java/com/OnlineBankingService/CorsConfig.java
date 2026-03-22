cfa48fcdd444e30a47278e2d9a23fe5961712a8d
Mikhail Nosov <misha.d.nosov@gmail.com>
2026-03-22 21:52:58 +0700

    +micro fix

--- src/main/java/com/OnlineBankingService/CorsConfig.java
+++ src/main/java/com/OnlineBankingService/CorsConfig.java
@@ -15,10 +15,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5500/**", "http://localhost:8084/**", "http://localhost:8085/**", "http://localhost:8081/**", "http://127.0.0.1:5500/**", "http://127.0.0.1:5501/**", "http://localhost:5173/**", "http://localhost:5172/**", "http://localhost:5174/**")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
